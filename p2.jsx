ecf41fa265e2f2f7b6749d598988a526227918a8
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-09-26 21:30:46 -0400

    added somefiles

---
+++ p2.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import States from "./components/states/States";
+
+ReactDOM.render(<States />, document.getElementById("reactapp"));
