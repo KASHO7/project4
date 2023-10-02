aec7d1ba029fcb93238f2842d29df8f8cf1fa9d9
aalqah13 <123989498+aalqah13@users.noreply.github.com>
2023-10-02 15:41:46 -0400

    Update gettingStarted.jsx

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import "./styles/main.css";
 
 import Example from "./components/example/Example";
+import Header from "./components/header/Header";)
 
 ReactDOM.render(<Example />, document.getElementById("reactapp"));
