0c431d54b51ea4d413efd171f3932f3a6528220c
yahmed0919 <112023093+yahmed0919@users.noreply.github.com>
2023-10-02 14:27:40 -0400

    update p5.jsx

--- p5.jsx
+++ p5.jsx
@@ -7,50 +7,26 @@ import Example from "./components/example/Example";
 import States from "./components/states/States";
 
 ReactDOM.render(<Header />, document.getElementById("reactappHeader"));
+const App = () => {
+    return (
+        <HashRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/states">States</Link>
+                        </li>
+                        <li>
+                            <Link to="/example">Example</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <hr />
+                <Route path="/states" component={States} />
+                <Route path="/example" component={Example} />
+            </div>
+        </HashRouter>
+    );
+};
 
-ReactDOM.render(
-  <HashRouter>
-    <div className="container">
-      <div className="menu">
-        <Link to="/example">Example</Link>
-      </div>
-      <div className="menu">
-        <Link to="/states">States</Link>
-      </div>
-    </div>
-    <Route path="/states" component={States} />
-    <Route path="/example" component={Example} />
-  </HashRouter>,
-  document.getElementById("reactapp")
-);
-
-// class Toggle extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {isToggleOn: true};
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-//
-//     handleClick() {
-//         this.setState(prevState => ({
-//             isToggleOn: !prevState.isToggleOn
-//         }));
-//     }
-//
-//     render() {
-//         return (
-//             <div className="container">
-//                 <button onClick={this.handleClick}>
-//                     {this.state.isToggleOn ? 'Switch to States' : 'Switch to Example'}
-//                 </button>
-//                 <hr />
-//                 {this.state.isToggleOn ? <Example /> : <States />}
-//             </div>
-//         );
-//     }
-// }
-//
-// ReactDOM.render(
-//     <Toggle />,
-//     document.getElementById('reactapp')
-// );
+ReactDOM.render(<App />, document.getElementById("reactapp"));
